95ed618e0de1093ca018efd2968033738d6fd2d7
Admin <kyawsoelinn24@gmail.com>
2025-12-04 20:42:13 +0630

    setup offline

---
+++ app/src/main/java/com/example/offline_todo/Model/TodoList.kt
@@ -0,0 +1,9 @@
+package com.example.offline_todo.Model
+
+data class TodoList(
+    val id: Int,
+    val title: String,
+    val description: String,
+    val date: String,
+    val time: String
+)
